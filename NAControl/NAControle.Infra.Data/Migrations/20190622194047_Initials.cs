b523774ef68bdc34ffc3b07a2f303c1ddf7b2268
Juliddanno Tghjeghjijueira Cortez Ga22rjuliacia <juliasnnotcg@hotghjmail.com>
2019-06-22 16:41:23 -0300

    Ajustes

---
+++ NAControl/NAControle.Infra.Data/Migrations/20190622194047_Initials.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NAControle.Infra.Data.Migrations
+{
+    public partial class Initials : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Endereco",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Logradouro = table.Column<string>(type: "VARCHAR(100)", nullable: false),
+                    Lote = table.Column<int>(type: "INT", nullable: false),
+                    Rua = table.Column<int>(type: "INT", nullable: false),
+                    Quadra = table.Column<int>(type: "INT", nullable: false),
+                    Cep = table.Column<int>(type: "INT", nullable: false),
+                    Cidade = table.Column<string>(type: "VARCHAR(100)", nullable: false),
+                    Uf = table.Column<string>(nullable: true),
+                    Latitude = table.Column<string>(type: "VARCHAR(30)", nullable: false),
+                    Longitude = table.Column<string>(type: "VARCHAR(30)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endereco", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grupo",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(type: "VARCHAR(50)", nullable: false),
+                    EnderecoId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grupo", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Grupo_Endereco_EnderecoId",
+                        column: x => x.EnderecoId,
+                        principalTable: "Endereco",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Grupo_EnderecoId",
+                table: "Grupo",
+                column: "EnderecoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Grupo");
+
+            migrationBuilder.DropTable(
+                name: "Endereco");
+        }
+    }
+}
